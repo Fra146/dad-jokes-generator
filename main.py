e9422d573f7196e6d3cf95ebb1230b56fbc9fd8a
Fra146 <francescocammarata.fr@gmail.com>
2024-06-18 04:16:22 +0200

    Improve general readability, add comments

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from coolprint import coolprint as cprint
 import json
 import random
+from coolprint import coolprint as cprint
 
-
-def get_next_line():
-    with open('jokes.json', 'r') as f:
-        jokes = json.load(f)
+def get_next_joke():
+    # Open the jokes.json file
+    with open('jokes.json', 'r') as file:
+        jokes = json.load(file)
     
-    # get a random joke
+    # Check if there are any jokes left
     if len(jokes) == 0:
         cprint('No more jokes found')
         return False
     
+    # Select a random joke
     joke = random.choice(jokes)
+    
+    # Print the joke
     if len(joke) == 2:
         cprint(f"\033[91m{joke[0]}\033[0m ")
         input()
@@ -21,13 +24,17 @@ def get_next_line():
     else:
         cprint(f"\033[92m{joke[0]}")
         cprint('\n\033[0m')
-    jokes.pop(jokes.index(joke))
+    
+    # Remove the joke from the list
     input()
 
 def check_config():
     try:
-        with open('jokes.json', 'r') as f:
-            jokes = json.load(f)
+        # Open the jokes.json file
+        with open('jokes.json', 'r') as file:
+            jokes = json.load(file)
+            
+            # Check if the jokes.json file is empty
             if not jokes:
                 print('jokes.json is empty')
                 return False
@@ -38,22 +45,24 @@ def check_config():
     except json.JSONDecodeError:
         print('jokes.json is not a valid JSON file')
         return False
-    
 
 def main():
     cprint('\033[1mWelcome to \033[4mProgramming Dad Jokes Generator\033[0m')
 
+    # Check the configuration
     if not check_config():
         return
+    
     cprint('Jokes loaded!')
-    cprint('\n\n\n')
     input('Generate a joke? (Press Enter to continue)')
-    cprint('\n\n\n')
+    cprint('\n\n')
+    
+    # Loop to generate jokes until there are no more left
     while True:
-        if get_next_line() == False:
+        if get_next_joke() == False:
             break
+    
     cprint('Goodbye!')
-
 
 if __name__ == '__main__':
     main()
